533ee9f72c2650a105095c2ec0b845f234ed0859
Brennan Guy Coheleach <Brennan.Coheleach1@gmail.com>
2020-07-05 18:02:01 -0400

    fixed outstanding interceptor bug

--- self-help/src/app/interceptors/firebase-interceptor.service.ts
+++ self-help/src/app/interceptors/firebase-interceptor.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
 import { SignInMethod } from '../enums/sign-in-method.enum';
@@ -14,15 +14,12 @@ export class FirebaseInterceptorService implements HttpInterceptor {
         req: HttpRequest<any>, 
         next: HttpHandler)
         : Observable<HttpEvent<any>> {
-            // return this.authService.user.pipe(take(1), exhaustMap(user => {
-            //     if(user != null) {
-            //         const alteredRequest: HttpRequest<any> = req.clone({setHeaders: {
-            //             auth: this.authService.user.value.idToken
-            //         }});
-            //         return next.handle(alteredRequest);
-            //     }
-            //     return next.handle(req);
-            // }));
-            return next.handle(req);
+            return this.authService.user.pipe(take(1), exhaustMap(user => {
+                if(user != null) {
+                    const alteredRequest = req.clone({params: new HttpParams().set('auth',user.idToken)});
+                    return next.handle(alteredRequest);
+                }
+                return next.handle(req);
+            }));
     }
 }
